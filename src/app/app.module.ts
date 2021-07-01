599c07536640640bd184e5c53d44cc43d50d43a0
Manav Misra <manavm@visionwebsoft.com>
2021-07-01 11:03:24 -0500

    :seedling: Generate courses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CourseCardComponent } from './components/courses/course-card/course-card.component';
+import { CoursesComponent } from './components/courses/courses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseCardComponent
+    CourseCardComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule
