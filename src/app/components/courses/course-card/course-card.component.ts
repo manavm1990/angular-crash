35e4605fa8f8c0909527a8d14623ac1b36a264dc
Manav Misra <manavm@visionwebsoft.com>
2021-07-01 11:28:00 -0500

    Render course cards 🎴

--- src/app/components/courses/course-card/course-card.component.ts
+++ src/app/components/courses/course-card/course-card.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Course } from 'src/models/course';
 
 @Component({
   selector: 'app-course-card',
   templateUrl: './course-card.component.html',
-  styleUrls: ['./course-card.component.css']
+  styleUrls: ['./course-card.component.css'],
 })
-export class CourseCardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class CourseCardComponent {
+  @Input() course!: Course;
 }
--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { COURSES } from 'src/db';
+import { Course } from 'src/models/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+  styleUrls: ['./courses.component.css'],
 })
 export class CoursesComponent implements OnInit {
+  courses: Course[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.courses = COURSES;
   }
-
 }
