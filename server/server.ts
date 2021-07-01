7f5d66c728ddecc1c08fa32c27a88724744509cd
Manav Misra <manavm@visionwebsoft.com>
2021-07-01 10:42:45 -0500

    :bug: Get server 🏃🏾‍♂️

--- server/server.ts
+++ server/server.ts
@@ -13,4 +13,6 @@ app.route('/api/courses').get(getAllCourses);
 
 app.route('/api/courses/:id').put(saveCourse);
 
-const httpServer = app.listen(9000);
+const httpServer = app.listen(9000, () => {
+  console.log(`Listening on port 9000`);
+});
