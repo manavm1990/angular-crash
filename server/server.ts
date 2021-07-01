bb0ff6a12ae00c822558743d30252020dcf19042
Manav Misra <manavm@visionwebsoft.com>
2021-07-01 04:48:09 -0500

    :seedling: Copy paste starter spit
    https://github.com/angular-university/angular-course/tree/1-components

---
+++ server/server.ts
@@ -0,0 +1,16 @@
+import * as express from 'express';
+import { Application } from 'express';
+import { getAllCourses } from './routes/get-courses.route';
+import { saveCourse } from './routes/save-course.route';
+
+const bodyParser = require('body-parser');
+
+const app: Application = express();
+
+app.use(bodyParser.json());
+
+app.route('/api/courses').get(getAllCourses);
+
+app.route('/api/courses/:id').put(saveCourse);
+
+const httpServer = app.listen(9000);
