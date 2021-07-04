36929301067bcb9a20cf45430837415c58e1977c
Manav Misra <manavm@visionwebsoft.com>
2021-07-04 13:53:49 -0500

    :wrench: Import/export router routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseCardComponent } from './courses/course-card/course-card.component';
 import { CoursesComponent } from './courses/courses.component';
-import { AppRoutingModule } from './app-routing.module';
+
+const routes: Routes = [];
 
 @NgModule({
   declarations: [AppComponent, CourseCardComponent, CoursesComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
+  exports: [RouterModule],
 })
 export class AppModule {}
