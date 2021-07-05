bc8fc92689420784ce2f01b6f8d5513d02de718f
Manav Misra <manavm@visionwebsoft.com>
2021-07-04 22:10:03 -0500

    :bug: Get server working with request query

--- server/routes/get-courses.route.ts
+++ server/routes/get-courses.route.ts
@@ -2,11 +2,15 @@ import { Request, Response } from 'express';
 import { COURSES } from '../../src/db';
 
 export function getAllCourses(req: Request, res: Response) {
-  res.status(200).json({ payload: Object.values(COURSES) });
+  if (req.query['id']) {
+    getCourseById(req, res);
+  } else {
+    res.status(200).json({ payload: Object.values(COURSES) });
+  }
 }
 
 export function getCourseById(req: Request, res: Response) {
-  const courseId = req.params['id'];
+  const courseId = req.query['id'];
 
   const courses: any = Object.values(COURSES);
 
